8d8ed398c48bd0755406cd7af86a7dbd6f57d40b
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-26 21:56:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,4 +11,12 @@ class App < Sinatra::Base
     @result = params[:number].to_i*params[:number].to_i
     "#{@result}"
   end
+  
+   get '/say/:number/:phrase' do
+    str = ""
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    @num.times { str += "#{@phrase}\n" }
+    str
+  end
 end
